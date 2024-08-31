2552d7863391e630e67081cc669f694ccc5f3769
geunhokinn <gghh1220@gmail.com>
2024-08-31 19:23:19 +0900

    Feat: SecurityConfig 작성 - 주석 달기 - 로그인 설정 - csrf 설정 해제

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -7,17 +7,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity // security 설정
+@EnableWebSecurity // security settings
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        return http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/", "/login").permitAll() // /, /login 에 대해 모두 접근 허용
-                .requestMatchers("/admin").hasRole("ADMIN") // /admin 에 대해 ADMIN role 을 가져야 접근 허용
-                .requestMatchers("/my/**").hasAnyRole("ADMIN","USER") // /my/** 에 대해 ADMIN, USER role 을 가져야 접근 허용
-                .anyRequest().authenticated()) // 나머지 경로에 대해 로그인한 사용자만 접근 허용
-                .build();
+        http
+                .authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/login").permitAll() // Allow all users access to /login and /
+                .requestMatchers("/admin").hasRole("ADMIN") // Allow user access with ADMIN role for /admin
+                .requestMatchers("/my/**").hasAnyRole("ADMIN","USER") // Allow user access with ADMIN, USER role for /my/**
+                .anyRequest().authenticated()); // Allow access to only logged-in users for the rest of the path
+
+
+        http
+                .formLogin((auth) -> auth.loginPage("/login") // Redirection to login page
+                        .loginProcessingUrl("/loginProc") // Login Processing
+                        .permitAll()); // Allow access to all
+
+
+        http
+                .csrf((auth) -> auth.disable()); // Unset csrf(cross site request forgery)
+
+        return http.build();
     }
 }
