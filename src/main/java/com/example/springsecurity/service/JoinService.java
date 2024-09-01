df685f99fd89ae51d0a1534df3dfdef0542936a7
geunhokinn <gghh1220@gmail.com>
2024-09-01 18:33:56 +0900

    Feat: MemberService 작석 - 동일한 username 을 검증하는 로직 추가

--- src/main/java/com/example/springsecurity/service/JoinService.java
+++ src/main/java/com/example/springsecurity/service/JoinService.java
@@ -21,6 +21,10 @@ public class JoinService {
     public void joinProcess(JoinDTO joinDTO) {
 
         // db 에 이미 동일한 username 을 가진 멤버가 존재하는지 검증
+        Boolean isMember = memberRepository.existsByUsername(joinDTO.getUsername());
+        if (Boolean.TRUE.equals(isMember)) {
+            throw new RuntimeException("동일한 아이디가 이미 존재합니다.");
+        }
 
         Member member = Member.buildMember(
                 joinDTO.getUsername(),
