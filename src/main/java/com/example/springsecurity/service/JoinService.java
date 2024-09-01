f9610722dc5a120912be197209b9c4b0893f9943
geunhokinn <gghh1220@gmail.com>
2024-09-01 18:21:22 +0900

    Feat: JoinService 작성

---
+++ src/main/java/com/example/springsecurity/service/JoinService.java
@@ -0,0 +1,33 @@
+package com.example.springsecurity.service;
+
+import com.example.springsecurity.dto.JoinDTO;
+import com.example.springsecurity.entity.Member;
+import com.example.springsecurity.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class JoinService {
+
+    private final MemberRepository memberRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public void joinProcess(JoinDTO joinDTO) {
+
+        // db 에 이미 동일한 username 을 가진 멤버가 존재하는지 검증
+
+        Member member = Member.buildMember(
+                joinDTO.getUsername(),
+                bCryptPasswordEncoder.encode(joinDTO.getPassword()), // One-way hashing processing
+                "ROLE_USER"
+        );
+
+        memberRepository.save(member);
+    }
+}
