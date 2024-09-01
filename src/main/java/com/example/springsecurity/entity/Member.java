7c8a4cccfe821d87cd9e92e5d7e544a0609b19d0
geunhokinn <gghh1220@gmail.com>
2024-09-01 18:20:26 +0900

    Feat: Member 작성 - 멤버 엔티티 작성

---
+++ src/main/java/com/example/springsecurity/entity/Member.java
@@ -0,0 +1,45 @@
+package com.example.springsecurity.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 아이디
+    @Column(nullable = false)
+    private String username;
+
+    // 비밀번호
+    @Column(nullable = false)
+    private String password;
+
+    // 사용자의 권한을 나타내기위한 role
+    @Column(nullable = false)
+    private String role;
+
+    @Builder
+    private Member(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static Member buildMember(String username, String password, String role) {
+        return Member.builder()
+                .username(username)
+                .password(password)
+                .role(role)
+                .build();
+    }
+}
+
