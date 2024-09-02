b7f996562e27e6bcb6fc494f29b6b21b2ae3912b
geunhokinn <gghh1220@gmail.com>
2024-09-02 11:08:53 +0900

    Feat: MainController 작성 - main view 에 id, role 을 넘겨주는 로직 추가

--- src/main/java/com/example/springsecurity/controller/MainController.java
+++ src/main/java/com/example/springsecurity/controller/MainController.java
@@ -1,13 +1,34 @@
 package com.example.springsecurity.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // id
+        String id = authentication.getName();
+
+        // role
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority grantedAuthority = iterator.next();
+        String role = grantedAuthority.getAuthority();
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
 
         return "main";
     }
